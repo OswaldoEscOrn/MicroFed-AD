a446dfb262fc74f7a5c2ea7f9e51d228c1dae622
YanzuLiu128 <412815213@qq.com>
2026-03-14 23:09:22 +0800

    Update convert_h5_to_tflite.py

--- tflite framework models built in ESP32 Platformio/convert_h5_to_tflite.py
+++ tflite framework models built in ESP32 Platformio/convert_h5_to_tflite.py
@@ -8,24 +8,24 @@ def convert_h5_to_tflite(h5_path, output_path):
 
     
 
-    # 启用 SELECT_TF_OPS 以支持 TensorList 操作
+    # Enable SELECT_TF_OPS to support TensorList operations
     converter.target_spec.supported_ops = [
         tf.lite.OpsSet.TFLITE_BUILTINS,
         tf.lite.OpsSet.SELECT_TF_OPS
     ]
     converter.allow_custom_ops = False
-    # 禁用降低 TensorList 操作
+    # Disable lowering of TensorList operations
     converter._experimental_lower_tensor_list_ops = False
     tflite_model = converter.convert()
     with open(output_path, 'wb') as f:
         f.write(tflite_model)
     print(f"Saved to {output_path}, size: {len(tflite_model)} bytes")
 
-# 输入路径
+# Input path
 conv_h5 = r"D:\Oswaldo's surf project\DR O's database\models\conv1d_autoencoder_multi_modal100k_data.h5"
 
 
-# 转换 Conv1D 模型
+# Convert Conv1D model
 convert_h5_to_tflite(conv_h5, "conv_100k_model.tflite")
 
 print("Done!")
